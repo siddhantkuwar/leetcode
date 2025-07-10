51e6e3ebd4f4dbec8b59373421234132988c6dd5
Siddhant Kuwar <94198354+siddhantkuwar@users.noreply.github.com>
2025-07-09 23:07:50 -0700

    Update best-time-to-buy-and-sell-stock.py

--- leetcode/best-time-to-buy-and-sell-stock.py
+++ leetcode/best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,14 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        left = 0    # buy
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0    # buy
+        right = 1   # sell
+        maxProfit = 0
 
-        while right < len(prices):
-        right = 1   # sell
-        maxProfit = 0
-            if prices[left] < prices[right]:
-                profit = prices[right] - prices[left]
-                maxProfit = max(maxProfit, profit)
-            else:
-                left = right
-            right += 1
-        return maxProfit
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+        return maxProfit
